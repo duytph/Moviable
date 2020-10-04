7a6f1ca3d0de9e4d24fc346d829d49d43ef4ec9d
Duy Tran <tphduy@gmail.com>
2020-10-05 01:57:19 +0700

    Integrate `AppCoordinator` to `SceneDelegate`

--- Movieable/App/SceneDelegate.swift
+++ Movieable/App/SceneDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let appCoordinator = DefaultAppCoordinator(window: window)
+        self.appCoordinator = appCoordinator
+        appCoordinator.start()
     }
 }
 
